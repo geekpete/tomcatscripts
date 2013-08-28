ee28a126758864ec68cedce5186713d6ab719e08
Peter Dyson <peter.dyson@geekpete.com>
2013-08-29 01:07:14 +1000

    updated readme and added a swathe of options to the tomcat init script

--- README.md
+++ README.md
@@ -2,3 +2,17 @@ tomcatscripts
 =============
 
 Tomcat Scripts
+
+Various scripts for Tomcat.
+
+pete@geekpete.com
+
+Notes on tomcat init script - tomcat.sh:
+-custom port prefix to allow multi tennancy of tomcat containers
+-debug_start for troubleshooting - enable jpda options
+-all config in one script at the top
+-lots of other example configs
+-optional email alert, not included, provided as separate snippet. Could be called on startup to alert of restart.
+-needs to set ulimit as correct user
+-still need to write force redeploy option, ensure exploded war dirs are wiped on restart, not an issue with deployer/manager.
+-
--- tomcat.sh
+++ tomcat.sh
@@ -12,7 +12,8 @@
 #
  
 # 
-# -JAVA_HOME - use default java - resolution is done dynamically
+# -Location of JAVA_HOME (bin files) 
+# either use default java - resolution is done dynamically
 export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:jre/bin/java::")
 
 # or set manually for a custom java version/location
@@ -23,12 +24,15 @@ export JAVA_HOME=$(readlink -f /usr/bin/java | sed "s:jre/bin/java::")
 export PATH=$JAVA_HOME/bin:$PATH
  
 #CATALINA_HOME is the location of the bin files of Tomcat  
-export CATALINA_HOME=/home/peter/Apps/apache-tomcat-7.0.42
+export CATALINA_HOME=/opt/apache-tomcat-7.0.42
  
 #CATALINA_BASE is the location of the configuration files of this instance of Tomcat
-export CATALINA_BASE=/home/peter/Apps/apache-tomcat-7.0.42
+# to create a new app config dir, copy your CATALINA_HOME to a new dir, eg. myapp1
+export CATALINA_BASE=/usr/share/myapp1
  
 #TOMCAT_USER is the default user of tomcat
+# if TOMCAT_USER is not present, falls back to running as root.
+# TODO: make tomcat fail if the required user is not available
 export TOMCAT_USER=peter
  
 #TOMCAT_USAGE is the message if this script is called without any options
@@ -37,6 +41,58 @@ TOMCAT_USAGE="Usage: $0 {\e[00;32mstart\e[00m|\e[00;31mstop\e[00m|\e[00;32mstatu
 #SHUTDOWN_WAIT is wait time in seconds for java proccess to stop
 SHUTDOWN_WAIT=20
  
+# Configure tomcat ports using a prefix
+PORT_PREFIX=80
+# Or use a custom port prefix:
+#PORT_PREFIX=100
+
+# automatically set the jvm route as the short host name, needs to be configured the same way on the front reverse proxy.
+JVM_ROUTE="`hostname -s`"
+
+# Force redeploy on startup - not yet implemented
+# Ensures the exploded war dirs in webapps are always wiped on startup, not an issue if using tomcat manager to deploy
+#FORCE_REDEPLOY=true
+
+# JAVA debug options (Java Platform Debug Architecture) used by Tomcat
+JPDA_TRANSPORT=
+JPDA_ADDRESS=${PORT_PREFIX}91
+JPDA_SUSPEND=
+JPDA_OPTS=
+export JPDA_TRANSPORT JPDA_ADDRESS JPDA_SUSPEND JPDA_OPTS
+
+# configure max threads for the container
+MAX_THREADS="200"
+
+# Configure ports using the port prefix specified above:
+SNMP_PORT=${PORT_PREFIX}16
+JMX_PORT=${PORT_PREFIX}45
+JMXRMI_PORT=${PORT_PREFIX}46
+HTTP_PORT=${PORT_PREFIX}80
+HTTPS_PORT=${PORT_PREFIX}43
+AJP_PORT=${PORT_PREFIX}09
+SHUTDOWN_PORT=${PORT_PREFIX}05
+
+LOGS_DIR="${CATALINA_BASE}/logs"
+
+# Set runtime parameters for custom ports, logs dir, catalina_base, max_threads.
+CUSTOM_PARAMETERS="-Dcustom.jvm.route=${JVM_ROUTE} -Dcustom.http.port=${HTTP_PORT} -Dcustom.https.port=${HTTPS_PORT} -Dcustom.ajp.port=${AJP_PORT} -Dcustom.shutdown.port=${SHUTDOWN_PORT} -Dcustom.logs.dir=${LOGS_DIR} -Dcustom.catalina.base=${CATALINA_BASE} -Dcustom.max.threads=${MAX_THREADS}"
+
+# Other options you can enable/configure here:
+#GC_LOG_OPTS="-XX:+PrintGCDetails -XX:+PrintGCDateStamps -XX:+PrintGCTimeStamps -Xloggc:${LOGS_DIR}/gc.log"
+#GC_OPTS="-XX:PermSize=64m -XX:MaxPermSize=256m -XX:+UseParallelGC -XX:-UseGCOverheadLimit"
+#MEM_OPTS="-Xms256m -Xmx2500m -Dsun.rmi.dgc.client.gcInterval=3600000 -Dsun.rmi.dgc.server.gcInterval=3600000 -XX:+HeapDumpOnOutOfMemoryError -XX:HeapDumpPath=$CUSTOM_LOGS_DIR"
+#JMX_OPTS="-Djmx.rmi.registry.port=${JMXRMI_PORT} -Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.port=${JMX_PORT} -Dcom.sun.management.jmxremote.ssl=false -Dcom.sun.management.jmxremote.authenticate=false"
+#OTHER_OPTS="-server -Dfile.encoding=utf8 -XX:+AggressiveOpts"
+#SSL_OPTS="-Djavax.net.ssl.trustStore=ssl/mytruststore.jks -Djavax.net.ssl.trustStorePassword=somepassword"
+#PROXY_OPTS="-Dhttp.nonProxyHosts=10.0.0.1|localhost -Dhttps.nonProxyHosts=10.0.0.1|whateversite.mydomain -Dhttp.proxyHost=myproxy.mydomain -Dhttp.proxyPort=3128"
+#APP_OPTS="-javaagent:$CATALINA_BASE/lib/org.springframework.instrument-3.0.2.RELEASE.jar -Dexternal.config.dir=${CATALINA_BASE}/config"
+
+# Glue together all the options that were set and export them as JAVA_OPTS
+export JAVA_OPTS="${CUSTOM_PARAMETERS} ${GC_OPTS} ${GC_LOG_OPTS} ${MEM_OPTS} ${JMX_OPTS} ${SSL_OPTS} ${APP_OPTS} ${PROXY_OPTS} ${OTHER_OPTS} ${SNMP_OPTS} "
+
+### End of configuration section ###
+
+#################################
 tomcat_pid() {
         echo `ps -fe | grep $CATALINA_BASE | grep -v grep | tr -s " "|cut -d" " -f2`
 }
@@ -88,7 +144,7 @@ stop() {
     done
  
     if [ $count -gt $kwait ]; then
-      echo -n -e "\n\e[00;31mkilling processes which didn't stop after $SHUTDOWN_WAIT seconds\e[00m"
+      echo -n -e "\n\e[00;31mkilling processes which didn't stop after $SHUTDOWN_WAIT seconds\e[00m\n"
       kill -9 $pid
     fi
   else
@@ -105,29 +161,23 @@ user_exists(){
                 echo "0"
         fi
 }
- 
+
 case $1 in
- 
         start)
-          start
+			start
         ;;
-       
         stop)  
-          stop
+			stop
         ;;
-       
         restart)
-          stop
-          start
+			stop
+			start
         ;;
-       
         status)
-                status
-               
+			status     
         ;;
-       
         *)
-                echo -e $TOMCAT_USAGE
+			echo -e $TOMCAT_USAGE
         ;;
 esac    
 exit 0
